da4c4d2b60bfc3a1a4401d84708c4afe39db9b0e
Jorge <jorgefco_rf@hotmail.com>
2021-05-16 03:11:04 -0700

    first commit

---
+++ YTdown.py
@@ -0,0 +1,33 @@
+from pytube import YouTube
+import os
+
+def downloadAudio(url):
+
+    salida = YouTube(url).streams.get_audio_only().download()
+    base, ext = os.path.splitext(salida)
+    nombre = base + ".mp3"
+    os.rename (salida, nombre)
+    print("\n¡Descargado!\nNombre del archivo: ",os.path.basename(salida))
+
+def downloadVideo(url):
+    salida = YouTube(url).streams.get_highest_resolution().download()
+    nombre = os.path.basename(salida)
+    print("\n¡Descargado!\nNombre del archivo: "+nombre)
+
+#Inicio
+
+print ("-----Bienvenido a YTdown!-----\n")
+print("1) Descargar audio\n2) Descargar audio y video\n")
+opc = int(input("Introduce una opción: "))
+
+while (True):
+    match opc:
+        case 1:
+            downloadAudio(input("Introduce la URL del video: "))
+
+        case 2:
+            downloadVideo(input("Introduce la URL del video: "))
+
+    loop = input("\nPresione enter para descargar otro archivo, o introduzca 0 para salir: ")
+    if (loop == "0"):
+        break
