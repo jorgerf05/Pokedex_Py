5d90f6bb3fc1ba7bef0e647bf2fe349d3f12c36a
Jorge Rodríguez <jorgerfco_rf@hotmail.com>
2021-05-17 01:34:23 -0700

    if elif

--- YTdown.py
+++ YTdown.py
@@ -23,12 +23,10 @@ while (True):
     print("1) Descargar audio\n2) Descargar audio y video\n")
     opc = int(input("Introduce una opción: "))
 
-    match opc:
-        case 1:
-            downloadAudio(input("Introduce la URL del video: "))
-
-        case 2:
-            downloadVideo(input("Introduce la URL del video: "))
+    if opc == 1:
+        downloadAudio(input("Introduce la URL del video: "))
+    elif opc ==2 :
+        downloadVideo(input("Introduce la URL del video: "))
 
     loop = input("\nPresione enter para descargar otro archivo, o introduzca 0 para salir: \n")
     if (loop == "0"):
